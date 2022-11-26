ecd0223f2e448ab226413e557f7e858e34400e6c
Aleksey Lytvyn <lcccplitvin@ukr.net>
2022-11-26 19:29:51 +0200

    pushTimer

--- js/script.js
+++ js/script.js
@@ -30,7 +30,45 @@
             })
         }
     });
+    //timer
 
+    const deadLine = '2022-11-27';
+
+    function getTimeRemaning(endtime){
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((t / 1000 / 60) % 60 ),
+              seconds = Math.floor((t / 1000) % 60 );
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'second': seconds
+        }
+    }
+    function setClock(selector, entime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000)
+
+        function updateClock(){
+            const t = getTimeRemaning(entime);
+            days.innerHTML = t.days,
+            hours.innerHTML = t.hours,
+            minutes.innerHTML = t.minutes,
+            seconds.innerHTML = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval)
+            }
+        }
+    }
+    setClock('.timer', deadLine)
 });
 
 
